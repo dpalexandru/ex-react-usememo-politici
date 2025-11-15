b8a440b510fe310ebf57137b05d4954212c88b4f
dpalexandru <alexpoian91@gmail.com>
2025-11-15 22:16:15 +0530

    [Mailstone2] searchbar and useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 
 
 function App() {
+  //stati
   const [listaPolitici, setListaPolitici] = useState([])
+  const [ricerca, setRicerca] = useState("");
 
   //recupero lista politici
   useEffect(() => {
@@ -11,37 +13,49 @@ function App() {
       .then(data => {
         console.log(data);
         setListaPolitici(data);
-
       })
       .catch(err => console.error(err));
 
   }, [])
 
 
-
   //componente card
-  function Card({ listaPolitici }) {
-
-    return listaPolitici.map((p, i) => (
-
+  function Card({ lista }) {
+    return lista.map((p, i) => (
       <div className="card" key={i} >
         <h2 className='card-title'>{p.name}</h2>
         <img className="card-img" src={p.image} alt="" />
         <p className='card-position' >Position: {p.position}</p>
         <p className='card-biography'>Biography: {p.biography}</p>
-
       </div>
     ))
-
-
   }
+
+
+  // Array politici filtrati con useMemo
+  const listaFiltrati = useMemo(
+    () => listaPolitici.filter((p) =>
+      p.name.toLowerCase().includes(ricerca.toLowerCase())
+      || p.biography.toLowerCase().includes(ricerca.toLowerCase())),
+    [listaPolitici, ricerca]
+  );
 
 
   return (
     <>
-      <h1 className='titolo-pagina'>Big Politicians</h1>
+      <header>
+        <h1 className='titolo-pagina'>Big Politicians</h1>
+        <input
+          placeholder='Cerca per nome o bio..'
+          className='capo-ricerca'
+          type="text"
+          value={ricerca}
+          onChange={e => setRicerca(e.target.value)}
+
+        />
+      </header>
       <div className="cards-container">
-        <Card listaPolitici={listaPolitici} />
+        <Card lista={listaFiltrati} />
       </div>
 
     </>
